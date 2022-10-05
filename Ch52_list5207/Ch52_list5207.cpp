2fe4e4fc9f7f859abd52a8d4e65f19bdcffe55c6
KeitaYamadaJapan <43283597+KeitaYamadaJapan@users.noreply.github.com>
2022-10-05 15:53:57 +0900

    Update Ch52_list5207.cpp

--- Ch52_list5207/Ch52_list5207.cpp
+++ Ch52_list5207/Ch52_list5207.cpp
@@ -7,6 +7,12 @@
 
 /** @file list5207.cxx */
 /** Listing 52-7. Trying to Mix rational Base Types */
+/**
+     Modifyed sample program from "Exploring C++20"
+        Auther: Ray Lischner 
+*/
+
+
 
 #include <iostream>
 #include <stdexcept>
