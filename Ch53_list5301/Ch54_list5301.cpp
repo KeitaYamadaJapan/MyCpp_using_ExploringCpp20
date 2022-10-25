5e021b2053082e2f6d05c95cea006b54e7d9f199
KeitaYamadaJapan <43283597+KeitaYamadaJapan@users.noreply.github.com>
2022-10-25 13:54:40 +0900

    Add files via upload

---
+++ Ch53_list5301/Ch54_list5301.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  Ch53_list5301
+//
+//  Created by 山田啓太 on 2022/10/07.
+//
+
+#include <iostream>
+
+/** @file list5301.hh */
+/** Listing 53-1. The is_void Class Template */
+template<class T>
+class is_void : public std::false_type
+{};
+
+template<>
+class is_void<void> : public std::true_type
+{};
+
+int main()
+{
+  std::cout << std::boolalpha <<
+    is_void<int>::value << '\n' <<
+    is_void<void>::value << '\n';
+}
