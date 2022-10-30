2dba3aae9b2b254eaceb80ef3becb658b1f3f868
KeitaYamadaJapan <43283597+KeitaYamadaJapan@users.noreply.github.com>
2022-10-30 11:01:06 +0900

    Add files via upload

---
+++ Ch02_list0203/Ch02_list0203.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Ch02_list0203
+//
+//  Created by 山田啓太 on 2022/08/23.
+//
+
+/** @file list0203.cpp */
+/** Listing 2-3. Determining the Number of Bits in a bool */
+#import <iostream>
+#import <limits>
+
+int main()
+{
+  // Note that "digits" means binary digits, i.e., bits.
+  std::cout << "bits per bool: " << std::numeric_limits<bool>::digits << '\n';
+}
