361b8da2d26e04920eace2fd300fbb7a7ca6691b
KeitaYamadaJapan <43283597+KeitaYamadaJapan@users.noreply.github.com>
2022-10-30 10:46:28 +0900

    Add files via upload

---
+++ Ch02_list0201/Ch02_list0201.cpp
@@ -0,0 +1,33 @@
+//
+//  main.cpp
+//  Ch02_list0201
+//
+//  Created by 山田啓太 on 2022/08/23.
+//
+
+/** @file list0201.cpp */
+/** Listing 2-1. Reading Test */
+/// Read the program and determine what the program does.
+
+#import <iostream>
+#import <limits>
+
+int main()
+{
+    int min{std::numeric_limits<int>::max()};
+    int max{std::numeric_limits<int>::min()};
+    bool any{false};
+    int x;
+    std::cout << "Input:" <<std::endl;
+    while (std::cin >> x)
+    {
+        any = true;
+        if (x < min)
+            min = x;
+        if (x > max)
+            max = x;
+    }
+
+    if (any)
+        std::cout << "min = " << min << "\nmax = " << max << '\n';
+}
