97873533afa52b2cc7b507d56637b0db010f6c65
KeitaYamadaJapan <43283597+KeitaYamadaJapan@users.noreply.github.com>
2022-10-30 10:48:00 +0900

    Update Ch02_list0201.cpp

--- Ch02_list0201/Ch02_list0201.cpp
+++ Ch02_list0201/Ch02_list0201.cpp
@@ -5,6 +5,14 @@
 //  Created by 山田啓太 on 2022/08/23.
 //
 
+/**
+     For my text.
+     Sample program from "Exploring C++20"
+        Auther: Ray Lischner 
+     Not modifyed.
+*/
+
+
 /** @file list0201.cpp */
 /** Listing 2-1. Reading Test */
 /// Read the program and determine what the program does.
